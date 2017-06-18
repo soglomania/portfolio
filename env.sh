628bab0d27b1e37194fc569689bb58aac04a19c2
sogloarcadius <rtsoglo@gmail.com>
2017-06-19 00:36:04 +0200

    test issue

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export AWS_STORAGE_BUCKET_NAME="soglomania-django-website-storage"
+export AWS_ACCESS_KEY_ID="AKIAIY24M6H4MCSLARVQ"
+export AWS_SECRET_ACCESS_KEY="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"
