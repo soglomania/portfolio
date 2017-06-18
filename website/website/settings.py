8296f04b0159adb40104f77a6fc9c2cf466a97c9
sogloarcadius <rtsoglo@gmail.com>
2017-06-18 16:24:49 +0200

    update settings aws

--- website/website/settings.py
+++ website/website/settings.py
@@ -141,38 +141,26 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-
-
-
-
 AWS_STORAGE_BUCKET_NAME = 'soglomania-django-website-storage'
 
 AWS_ACCESS_KEY_ID = 'AKIAIGMBLEY4NYMO2XCQ'
 
 AWS_SECRET_ACCESS_KEY = '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'
 
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_LOCATION = 'static'
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-MEDIAFILES_LOCATION = 'media'
-
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-
-
-from storages.backends.s3boto import S3BotoStorage
-
-class StaticStorage(S3BotoStorage):
-    location = STATICFILES_LOCATION
-
-class MediaStorage(S3BotoStorage):
-    location = MEDIAFILES_LOCATION
-
-STATICFILES_STORAGE = StaticStorage
-
-DEFAULT_FILE_STORAGE = MediaStorage
 
 
 
