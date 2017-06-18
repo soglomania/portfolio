1d47934b0556d9cc1d092c57032bdfaacf035285
sogloarcadius <rtsoglo@gmail.com>
2017-06-18 10:55:16 +0200

    collect static files

---
+++ website/static/home/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('select').material_select();
+    $('.collapsible').collapsible();
+    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
