b62846a38161f1782eb576264a432d3da16893bb
sogloarcadius <rtsoglo@gmail.com>
2017-07-03 23:33:03 +0200

    minor enhancements to templates

--- website/website/urls.py
+++ website/website/urls.py
@@ -30,7 +30,7 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
         url(r'^translate/', include('rosetta.urls')),
+        url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
 
