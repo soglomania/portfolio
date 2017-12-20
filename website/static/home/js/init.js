25c10c035ca10f8b6933f8d3ab0e38ca70bedfb6
sogloarcadius <rtsoglo@gmail.com>
2017-12-20 23:33:34 +0000

    update ignore

--- website/static/home/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('select').material_select();
-    $('.collapsible').collapsible();
-    
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
