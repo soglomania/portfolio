c5e33863b4033b96bcb59bf037d8034332f6e56e
sogloarcadius <rtsoglo@gmail.com>
2020-04-23 19:42:59 +0200

    update urls disable admin

--- app/portfolio/urls.py
+++ app/portfolio/urls.py
@@ -28,6 +28,6 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-        url(r'^admin/', include(admin.site.urls)),
+        url(r'^admin/', admin.site.urls),
     ] + urlpatterns
 
